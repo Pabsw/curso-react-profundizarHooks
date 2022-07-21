72d2194fc12c664b5978e1e2b942f8712b9e9492
Pablo Anastasio <pabloanastasio4326@gmail.com>
2022-07-21 20:03:06 +0200

    Primer commit

---
+++ src/06-memos/MemoHook.jsx
@@ -0,0 +1,33 @@
+import { useMemo, useState } from 'react';
+import {useCounter} from '../hooks';
+
+const heavyStuff = (iN=100) => {
+  for(let i = 0; i<iN;i++){
+      console.log('Contando');
+  }
+
+  return `${iN} iteracciones realizadas`;
+}
+
+export const MemoHook = () => {
+
+    const {counter, increment} = useCounter(4000);
+    const [show, setshow] = useState(true);
+
+    const memorizedValue = useMemo(()=> heavyStuff(), [counter]);
+
+  return (
+    <>
+        <h1>Counter: <small>{counter}</small></h1>
+        <hr></hr>
+
+        <h4>{memorizedValue}</h4>
+
+        <button className='btn btn-primary' 
+          onClick={() => increment()}>+1</button>
+        <button className='btn btn-outline-primary' 
+          onClick={() => setshow(!show)}>Show/Hide{JSON.stringify(show)}</button>
+    
+    </>
+  )
+}
---
+++ src/08-useReducer/TodoApp.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { useTodos } from '../hooks/useTodos';
+import { TodoAdd } from './TodoAdd';
+import { TodoList } from './TodoList';
+
+
+
+export const TodoApp = () => {
+
+   const { todos, todosCount, pendingTodosCount, handleDeleteTodo, handleNewTodo, handleToggleTodo } = useTodos();
+  
+  return (
+    <>
+        <h1>TodoApp: {todosCount} <small>pendientes: {pendingTodosCount}</small></h1>
+        <hr></hr>
+
+        <div className="row">
+            <div className='col-7'>
+                <TodoList 
+                todos={todos} 
+                onDeleteTodo={handleDeleteTodo}
+                onToggleTodo={handleToggleTodo}/>
+            </div>
+
+            <div className='col-5'>
+                <h4>Agregar TODO</h4>
+                <hr></hr>
+                <TodoAdd onNewTodo={handleNewTodo}/>
+            </div>
+        </div>
+
+
+
+    </>
+  )
+}
